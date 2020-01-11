28a555434aba5b1e68a179c49de7c00e763aec38
perinazzoo <abcdperinazzoo@users.noreply.github.com>
2020-01-11 11:03:25 -0300

    update connect

--- server.js
+++ server.js
@@ -10,14 +10,8 @@ app.use(cors());
 
 // ? Iniciando o banco de dados
 
-const MongoClient = require("mongodb").MongoClient;
-const uri =
-  "mongodb+srv://perinazzoo:<seenha123>@cluster0-cewae.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true
 });
 
 requireDir("./src/models");
